22cc4dccbeb005f95e687e27e340c346b581777b
mcarman <carman.md@live.com>
2015-03-23 21:33:20 -0600

    implement post page

--- blogengine/migrations/0001_initial.py
+++ blogengine/migrations/0001_initial.py
@@ -8,13 +8,31 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Post'
+        db.create_table(u'blogengine_post', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('text', self.gf('django.db.models.fields.TextField')()),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=40)),
+        ))
+        db.send_create_signal(u'blogengine', ['Post'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Post'
+        db.delete_table(u'blogengine_post')
+
 
     models = {
-        
+        u'blogengine.post': {
+            'Meta': {'ordering': "['-pub_date']", 'object_name': 'Post'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '40'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
     }
 
     complete_apps = ['blogengine']
--- blogengine/tests.py
+++ blogengine/tests.py
@@ -14,16 +14,21 @@ class PostTest(TestCase):
         post.title = 'My first post'
         post.text = 'This is my first blog post'
         post.pub_date = timezone.now()
+        post.slug = 'my-first-post'
+
         # Save it
         post.save()
+
         # Check we can find it
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 1)
         only_post = all_posts[0]
         self.assertEquals(only_post, post)
+
         # Check attributes
         self.assertEquals(only_post.title, 'My first post')
         self.assertEquals(only_post.text, 'This is my first blog post')
+        self.assertEquals(only_post.slug, 'my-first-post')
         self.assertEquals(only_post.pub_date.day, post.pub_date.day)
         self.assertEquals(only_post.pub_date.month, post.pub_date.month)
         self.assertEquals(only_post.pub_date.year, post.pub_date.year)
@@ -87,22 +92,15 @@ class AdminTest(LiveServerTestCase):
         response = self.client.get('/admin/blogengine/post/add/')
         self.assertEquals(response.status_code, 200)
 
-    def test_create_post(self):
-        # Log in
-        self.client.login(username='bobsmith', password="password")
-
-        # Check response code
-        response = self.client.get('/admin/blogengine/post/add/')
-        self.assertEquals(response.status_code, 200)
-
         # Create the new post
         response = self.client.post('/admin/blogengine/post/add/', {
             'title': 'My first post',
             'text': 'This is my first post',
             'pub_date_0': '2013-12-28',
-            'pub_date_1': '22:00:04'
+            'pub_date_1': '22:00:04',
+            'slug': 'my-first-post'
         },
-        follow=True
+            follow=True
         )
         self.assertEquals(response.status_code, 200)
 
@@ -129,9 +127,10 @@ class AdminTest(LiveServerTestCase):
             'title': 'My second post',
             'text': 'This is my second blog post',
             'pub_date_0': '2013-12-28',
-            'pub_date_1': '22:00:04'
+            'pub_date_1': '22:00:04',
+            'slug': 'my-second-post'
         },
-        follow=True
+            follow=True
         )
         self.assertEquals(response.status_code, 200)
 
@@ -150,6 +149,7 @@ class AdminTest(LiveServerTestCase):
         post = Post()
         post.title = 'My first post'
         post.text = 'This is my first blog post'
+        post.slug = 'my-first-post'
         post.pub_date = timezone.now()
         post.save()
 
@@ -183,6 +183,7 @@ class PostViewTest(LiveServerTestCase):
         post = Post()
         post.title = 'My first post'
         post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
+        post.slug = 'my-first-post'
         post.pub_date = timezone.now()
         post.save()
 
@@ -192,6 +193,42 @@ class PostViewTest(LiveServerTestCase):
 
         # Fetch the index
         response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check the post title is in the response
+        self.assertTrue(post.title in response.content)
+
+        # Check the post text is in the response
+        self.assertTrue(markdown.markdown(post.text) in response.content)
+
+        # Check the post date is in the response
+        self.assertTrue(str(post.pub_date.year) in response.content)
+        self.assertTrue(post.pub_date.strftime('%b') in response.content)
+        self.assertTrue(str(post.pub_date.day) in response.content)
+
+        # Check the link is marked up properly
+        self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+    def test_post_page(self):
+        # Create the post
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
+        post.slug = 'my-first-post'
+        post.pub_date = timezone.now()
+        post.save()
+
+        # Check new post saved
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+        only_post = all_posts[0]
+        self.assertEquals(only_post, post)
+
+        # Get the post URL
+        post_url = only_post.get_absolute_url()
+
+        # Fetch the post
+        response = self.client.get(post_url)
         self.assertEquals(response.status_code, 200)
 
         # Check the post title is in the response
